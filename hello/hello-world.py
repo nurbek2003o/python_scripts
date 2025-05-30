d9d0ca915bbdf4e0aa29d9786791b2e4601d1693
Nurbek Eshquvvatov <nurbekeshquvvatov22@gmail.com>
2025-05-30 21:01:46 +0500

    add new files

--- hello/hello-world.py
+++ hello/hello-world.py
@@ -1,13 +1,43 @@
-kocha = input("Ko'cha nomini  kiriting\n")
-mahalla = input("Mahalla\n")
-tuman = input("Tuman\n")
-viloyat = input("Viloyat\n")
+# kocha = input("Ko'cha nomini  kiriting\n")
+# mahalla = input("Mahalla\n")
+# tuman = input("Tuman\n")
+# viloyat = input("Viloyat\n")
 # print(kocha + " " + "ko'chasi,\n", mahalla + " " + "mahallasi,\n", tuman + " " + "tumani,\n", viloyat + " " + "viloyati." )
-manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
-print(manzil.upper())
-print(manzil.lower())
-print(manzil.title())
-print(manzil.capitalize())
-print(manzil.lstrip())
-print(manzil.rstrip())
-print(manzil.strip())
+# manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+# print(manzil.upper())
+# print(manzil.lower())
+# print(manzil.title())
+# print(manzil.capitalize())
+# print(manzil.lstrip())
+# print(manzil.rstrip())
+# print(manzil.strip())
+
+
+#  Sonlar darsi vazifasi
+
+# 1-vazifa
+
+# a =  int(input("Istalgan sonni kiriting\n"))
+# b = a**2
+# c = a**3
+# print(str(a) + ' ' + 'ning kvadrati' + ' ' + str(b) + ' ' +'ga teng')
+# print(str(a) + ' ' + 'ning kubi' + ' ' + str(c) + ' ' +'ga teng')
+
+# 2-vazifa
+# a = int(input("Yoshingiz nechida?\n"))
+# b = 2025 - a
+# print("Siz" + " "  + str(b) + " " + "da tug'ilgansiz ")
+
+# 3-vazifa
+a = str(float(input("Birinchi sonni kiriting:")))
+b = str(float(input("Ikkinchi sonni kiriting:")))
+c = str(float(a) + float(b))
+d = str(float(a) - float(b))
+e = str(float(a) * float(b))
+f = str(float(a) / float(b))
+
+print(a + ' ' + '+' + ' ' + b + ' =' + ' ' + c)
+# print(float(c))
+print(a + ' ' + '-' + ' ' + b + ' =' + ' ' + d)
+print(a + ' ' + '*' + ' ' + b + ' =' + ' ' + e)
+print(a + ' ' + '/' + ' ' + b + ' =' + ' ' + f)
