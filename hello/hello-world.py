03ef8075fd545d501c7c2408430870d02059f690
Nurbek Eshquvvatov <nurbekeshquvvatov22@gmail.com>
2025-07-02 17:09:04 +0500

    20-dars vazifalari

--- hello/hello-world.py
+++ hello/hello-world.py
@@ -988,9 +988,158 @@ davlatlar={
 #     print(f"{x} ning {y}-darajasi {x**y}ga teng")
 # son_daraja(9)
 
-def son_bolinish(son):
-    son=int(input("Ixtiyoriy sonni kiriting: "))
-    for n in range(2,10):
-        if son%n==0:
-            print(f"{son} {n} ga qoldiqsiz bo'linadi")
-son_bolinish(5)
+# def son_bolinish(son):
+#     son=int(input("Ixtiyoriy sonni kiriting: "))
+#     for n in range(2,10):
+#         if son%n==0:
+#             print(f"{son} {n} ga qoldiqsiz bo'linadi")
+# son_bolinish(5)
+
+
+#20-dars
+
+# def toliq_ism_yasa(ism, familiya, otasining_ismi=''):
+#     """To'liq ism qaytaruvcji funksiya"""
+#     if otasining_ismi:
+#         toliq_ism=f"{ism} {familiya} {otasining_ismi}"
+#     else:
+#         toliq_ism=f"{ism} {familiya}"
+#     return toliq_ism
+# talaba1=toliq_ism_yasa('Hakim', 'Salimov')
+# talaba2=toliq_ism_yasa('qochqar', 'qoziyev', 'boltayevich')
+# print(talaba1.title())
+# print(talaba2.title())
+
+# def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+#     avto={
+#         'kompaniya':kompaniya,
+#         'model':model,
+#         'rangi':rangi,
+#         'korobka':korobka,
+#         'yili':yili,
+#         'narhi':narhi
+#     }
+#     return avto
+# avto1=avto_info('gm','malibu','qora', 'avtomat', 2018)
+# avto2=avto_info('gm','lasetti', 'oq','mexanika',2020, 18000)
+# avtolar=[avto1,avto2]
+# print('Onlayn bozordagi mavjud mashinalar:')
+#
+# for avto in avtolar:
+#     if avto['narh']:
+#         narh=avto['narh']
+#     else:
+#         narh='Nomalum'
+#     print(f"{avto['rangi'].title()} {avto['model'].title()}. Narhi:{narh}")
+
+# def oraliq(min,max, qadam=''):
+#     sonlar=[]
+#     while min<max:
+#         sonlar.append(min)
+#         if qadam != '':
+#             min +=qadam
+#         else:
+#             min+=1
+#     return sonlar
+# print(oraliq(0,100,10))
+#def avto_info(kompaniya,model,rangi,korobka,yili,narhi):
+# print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
+# avtolar = []  # salondagi avtolar uchun bo'sh ro'yxat
+# while True:
+#     print("Quyidagi ma'lumotlarni kiriting\n")
+#     kompaniya = input("Ishlab chiqaruvchi: ")
+#     model = input("Modeli: ")
+#     rangi = input("Rangi: ")
+#     korobka = input("Korobka: ")
+#     yili = input("Ishlab chiqarilgan yili: ")
+#     narhi = input("Narhi: ")
+#
+#     Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida
+#     lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
+    # avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
+    #
+    # Yana avto qo'shish-qo'shmaslikni so'raymiz
+    # javob = input("Yana avto qo'shasizmi? (yes/no): ")
+    # if javob == 'no':
+    #     break
+#
+# print(avtolar)
+
+
+# Uyga vazifalar
+# def toliq_ism_yasa(ism,familiya,tyil,tjoy,email='',tel=''):
+#         toliq_ism={
+#             'ism':ism,
+#             'familiya':familiya,
+#             'tyil':tyil,
+#             'yoshi': 2025 - tyil,
+#             'tjoy':tjoy,
+#             'email':email,
+#             'tel':tel
+#         }
+#         return toliq_ism
+#
+# print("Quyidagi ma'lumotlarni kiriting: ")
+# toliq_nom=[]
+# while True:
+#     ism=input("Ismingizni kiriting: ")
+#     familiya=input("familyangiz: ")
+#     tyil=int(input("tug'ilgan yil: "))
+#     tjoy=input("tug'ilgan joy: ")
+#     email=input("email: ")
+#     tel=input("telefon: ")
+#     toliq_nom.append(toliq_ism_yasa(ism, familiya, tyil, tjoy, email, tel))
+#     javob=input("Davom etasizmi? (ha/yoq): ")
+#     if javob=='yoq':
+#         break
+# print(toliq_nom)
+# for nom in toliq_nom:
+#     print(
+#         f"{nom['ism'].title()} {nom['familiya'].title()},"
+#         f"{nom['yoshi']} yoshda, telefoni: {nom['tel']}"
+#     )
+
+# def kattasi(x,y,z):
+#     max=x
+#     if y>=max:
+#         max=y
+#     if z>=max:
+#         max=z
+#     return max
+
+# def aylana(radius,pi=3.14):
+#     aylana={
+#         'radius':radius,
+#         'diametr':2*radius,
+#         'perimetr':2*radius*pi,
+#         'yuzi':pi*radius**2
+#     }
+#     return aylana
+
+
+# def tub_sonlar_top(min,max):
+#     tub_sonlar=[]
+#     for n in range(min,max+1):
+#         tub=True
+#         if n==1:
+#             tub=False
+#         elif n==2:
+#             tub=True
+#         else:
+#             for x in range(2,n):
+#                 if n %x==0:
+#                     tub=False
+#         if tub:
+#             tub_sonlar.append(n)
+#         return tub_sonlar
+# tub_sonlar_top(2,15)
+
+# def son(n):
+#     sonlar=[]
+#     for x in range(n):
+#         if x==0 or x==1:
+#             sonlar.append(1)
+#         else:
+#             sonlar.append(sonlar[x-1]+sonlar[x-2])
+#         return sonlar
+# print(son(20))
